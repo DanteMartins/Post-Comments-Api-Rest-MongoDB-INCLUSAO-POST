0625d5b2fa36d3cef49d26f647c49d9a3cd48ea3
Dante Martins <Dante.Martins@yahoo.com.br>
2020-05-11 15:37:14 -0300

    Project Created

---
+++ src/main/java/com/dantemartins/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,14 @@
+package com.dantemartins.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//versão com inclusão do metodo Post para os post(entidade post)
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+}
